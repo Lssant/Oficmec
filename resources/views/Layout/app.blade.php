5feb0d98fe5313d6dd44c063f7371d8e433fe66e
Lssant <lssant@outlook.com>
2018-08-24 17:01:20 -0300

    iniciando navbar

--- resources/views/Layout/app.blade.php
+++ resources/views/Layout/app.blade.php
@@ -1,15 +1,18 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Oficina Mecânica</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content=" {{ csrf_token() }}" >
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1"> -->
     <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/app.css') }}" />
     <!-- <script src="main.js"></script> -->
 </head>
 <body>
     <div class="container">
+        @component('componente_navbar')
+        @endcomponent
         <main role="main">
             @hasSection('body')
                 @yield('body')
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,34 @@
 @extends('layout.app')
 
 @section('body')
-    <h1>Hello world</h1>
+    <div class="jumbotron bg-ligth border border-secondary">
+        <div class="cols">
+                <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                          <a class="nav-link active" href="#">Active</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                      </ul>
+            <div class="card-deck">
+                <div class="card border border-primary">
+                    <div class="card-body">
+
+                        <h5 class="card-title"> Cadastro </h5>
+                        <p class="card=text">
+                            Aqui cadstra os produtos
+                        </p>
+                        <a href="#" class="btn btn-primary">cadastre</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
