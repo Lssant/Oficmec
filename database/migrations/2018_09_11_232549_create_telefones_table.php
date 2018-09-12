d60bedc3baecbd318d503a86fbbc7f50250ead9a
Lssant <lssant@outlook.com>
2018-09-11 22:06:53 -0300

    migration cli e mec

---
+++ database/migrations/2018_09_11_232549_create_telefones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTelefonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telefones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('numero');
+            $table->integer('tipo_telefone_id')->unsigned()->nullable();
+            $table->foreing('tipo_telefone_id')->references('id')->on('tipo_telefones');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telefones');
+    }
+}
