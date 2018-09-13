8d274b49aa141ec2084be17b027dbf4552e54e5a
Lssant <lssant@outlook.com>
2018-09-12 21:07:41 -0300

    (inicio) seeder Pessoa/end/tel

---
+++ database/seeds/TipoTelefoneSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TipoTelefoneSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
