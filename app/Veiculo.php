7096684eca92d4c143e7275ac683810d6da37528
Lssant <lssant@outlook.com>
2018-10-25 00:25:29 -0300

    migrate veiculo

---
+++ app/Veiculo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Veiculo extends Model
+{
+    protected $fillable = [
+        'marca',
+        'modelo',
+        'cor',
+        'ano',
+        'combustivel',
+        'placa'
+    ];
+}
